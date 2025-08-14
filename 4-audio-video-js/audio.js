4ec883906ca6b47aff46b5d45ff8c3a19ea05ba5
klcb19 <kerencaldera16@gmail.com>
2025-08-14 15:32:54 -0500

    Adio-video1

---
+++ 4-audio-video-js/audio.js
@@ -0,0 +1,7 @@
+const video = document.querySelector("#video")
+const PlayVideo = document.querySelector("#play-video")
+const StopVideo = document.querySelector("#stop-video")
+const PlaystopImg = document.querySelector("#play-stop-img1")
+const PlaystopImg2 = document.querySelector("#play-stop-img2")
+ 
+console.log(video,PlayVideo,StopVideo,PlaystopImg,PlaystopImg2)

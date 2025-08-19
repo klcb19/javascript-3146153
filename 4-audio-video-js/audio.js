7582a3712495d621c20c01372433c0a2125e19a5
klcb19 <kerencaldera16@gmail.com>
2025-08-19 15:28:50 -0500

    audio2

--- 4-audio-video-js/audio.js
+++ 4-audio-video-js/audio.js
@@ -2,6 +2,50 @@ const video = document.querySelector("#video")
 const PlayVideo = document.querySelector("#play-video")
 const StopVideo = document.querySelector("#stop-video")
 const PlaystopImg = document.querySelector("#play-stop-img1")
+const Audio2 = document.querySelector("#audio-1-loronicol")
 const PlaystopImg2 = document.querySelector("#play-stop-img2")
- 
-console.log(video,PlayVideo,StopVideo,PlaystopImg,PlaystopImg2)
+const Audio1 = document.querySelector("#audio-1-perro")
+
+
+
+function ReproducirVideo() {
+    video.play();
+
+}
+PlayVideo.addEventListener("click", ReproducirVideo);
+
+function PausarVideo() {
+    video.pause();
+
+}
+StopVideo.addEventListener("click", PausarVideo)
+
+
+
+function toggleAudioUno() {
+
+    if (Audio1.paused) {
+
+        Audio1.play()
+        PlaystopImg2.textContent = "stop"
+    } else {
+        Audio1.pause()
+        PlaystopImg2.textContent = "play"
+    }
+}
+
+PlaystopImg.addEventListener("click", toggleAudioDos);
+
+function toggleAudioDos() {
+
+    if (Audio2.paused) {
+
+        Audio2.play()
+        PlaystopImg.textContent = "stop"
+    } else {
+        Audio2.pause()
+        PlaystopImg.textContent = "play"
+    }
+}
+
+PlaystopImg2.addEventListener("click", toggleAudioDos);

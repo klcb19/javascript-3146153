7502f73970b102a801b994261a4cdf2577e7acc5
klcb19 <kerencaldera16@gmail.com>
2025-08-25 15:45:27 -0500

    Add files via upload
    DOS

---
+++ 5_galeria_viñetas-dos/app.js
@@ -0,0 +1,73 @@
+
+const monedas = document.querySelectorAll(".coin");
+const corazones = document.querySelectorAll(".corazon");
+const contadorMonedas = document.querySelector("#contador-monedas");
+const contadorCorazones = document.querySelector("#contador-corazones");
+let totalMonedas = 0;
+let totalCorazones = 0;
+
+/* console.log(monedas, corazon, contadorMonedas, contadorCorazones); */
+
+monedas.forEach(function (moneda) {
+
+    moneda.addEventListener("click", function () {
+        moneda.classList.add("saltar");
+        totalMonedas++
+        contadorMonedas.textContent = totalMonedas
+
+    })
+
+})
+
+corazones.forEach(function (corazon) {
+
+    corazon.addEventListener("click", function () {
+        corazon.classList.add("saltar");
+        totalCorazones++;
+        contadorCorazones.textContent = totalCorazones;
+    })
+})
+
+
+
+///Galeria
+
+const escenas = document.querySelectorAll('.escena')
+const btAnterior = document.querySelector('.anterior')
+const btSiguiente = document.querySelector('.siguiente')
+const miniaturas = document.querySelectorAll('.miniaturas img')
+let indice = 0
+
+//console.log(escenas)
+//console.log(escenas[1])
+
+function mostrarEscena(i) {
+    // console.log(escenas[i])
+    for (let j = 0; j < escenas.length; j++) {
+        escenas[j].classList.remove("activa");
+        
+    }
+    escenas[i].classList.add("activa")
+
+    indice = i
+}
+
+//boton siguiente
+btSiguiente.addEventListener("click", function(){
+    indice = indice + 1
+    if(indice >= escenas.length){
+        indice = 0 //para que vuelva
+    }
+    mostrarEscena(indice)
+})
+
+//boton anterior
+btAnterior.addEventListener("click", function(){
+    indice = indice - 1
+    
+    if(indice < 0){
+        indice = escenas.length - 1
+    }
+    mostrarEscena(indice)
+})
+

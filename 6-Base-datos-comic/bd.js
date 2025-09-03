9dd8375cc725d0b6104db5e4c9ba64f85eabc566
klcb19 <kerencaldera16@gmail.com>
2025-09-03 15:36:19 -0500

    base de datos

---
+++ 6-Base-datos-comic/bd.js
@@ -0,0 +1,31 @@
+const comic = {
+    "nombreComic": "El Calentamiento Global y la Industralizacion",
+    "categoria" : "Distopico",
+    "cantidadCap" : 3,
+    "descripcion" : "Trata de un apocalipsis por plantas mutantes",
+    "personajes" : ["Ossian", "Asher", "Sora"],
+    "capitulos" :[
+        {
+            nombreCap: "Semilla del abandono",
+            imgPortada: "assets/cap1.jpg",
+            video: "source/videosCap1.mp4",
+            descripcion: "Comienza el apocalipsis, Asher se queda solo y muere la familia de Ossian",
+            personajes: ["Ossian", "Asher", "Sora"]
+        },
+        {
+            nombreCap: "El eco de la furia",
+            imgPortada: "assets/cap2.jpg",
+            video: "source/videosCap2.mp4",
+            descripcion: "Ossian se encuentra con Asher mutante y descubre verdades",
+            personajes: ["Ossian", "Asher"]
+        },
+        {
+            nombreCap: "El ocaso de la humanidad",
+            imgPortada: "assets/cap3.jpg",
+            video: "source/videosCap3.mp4",
+            descripcion: "Asher y Ossian trabajan para recuperar la humanidad",
+            personajes: ["Ossian", "Asher"]
+        }
+    ]
+
+}
